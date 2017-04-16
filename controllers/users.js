8b5b394aba1854b199c8745798c45a9c55a391c6
Lenin Yang <leninyang@gmail.com>
2017-04-15 18:36:47 -0700

    created user show and edit page

--- controllers/users.js
+++ controllers/users.js
@@ -10,19 +10,38 @@ var bcrypt = require('bcrypt');
 // ROUTES
 //=========================
 
-// 1) NEW ROUTE / Create User New Page
+// 1) USERS NEW ROUTE / Create User New Page
 router.get('/new', function(req, res) {
   res.render('users/new.ejs')
 });
 
-// 3) INDEX ROUTE
+// 3) USERS INDEX ROUTE
 router.get('/', function(req, res) {
   User.find({}, function(err, foundUsers) {
     res.render('users/index.ejs', {
       users: foundUsers
-    })
-  })
-})
+    });
+  });
+});
+
+// 4) USERS SHOW PAGE
+router.get('/:id', function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    res.render('users/show.ejs', {
+      user: foundUser
+    });
+  });
+});
+
+// 6) EDIT ROUTE
+router.get('/:id/edit', function(req, res) {
+  User.findById(req.params.id, function(err, foundUser) {
+    res.render('users/edit.ejs', {
+      user: foundUser
+    });
+  });
+});
+
 
 // 2) CREATE ROUTE | Create user data in DataBase
 router.post('/', function(req, res){
@@ -32,5 +51,11 @@ router.post('/', function(req, res){
   });
 });
 
+// 5) DELETE ROUTE
+router.delete('/:id', function(req, res) {
+  User.findByIdAndRemove(req.params.id, function() {
+    res.redirect('/users')
+  });
+});
 
 module.exports = router;
