fc2aa47001f8a728835d26ab8324dae9df6aa0b3
Lenin Yang <leninyang@gmail.com>
2017-04-17 12:29:36 -0700

    created new page for recipes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -5,6 +5,15 @@ var express = require('express');
 var router = express.Router();
 var Recipe = require('../models/recipes.js');
 
+//=========================
+// ROUTES
+//=========================
+
+// 2) RECIPES NEW ROUTE | CREATE NEW RECIPES
+router.get('/new', function(req, res) {
+  res.render('recipes/new.ejs');
+});
+
 // 1) RECIPES INDEX ROUTE
 router.get('/', function(req, res) {
   Recipe.find({}, function(err, foundRecipes) {
